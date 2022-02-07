3b9205b662c1dab62affb81d5580d10f2803d99c
hayden brown <haydenbrowncinci@gmail.com>
2022-02-07 09:49:33 -0500

    Add project files.

---
+++ BankingProject/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProject
+{
+    public class Account
+    {   //Properties
+        public decimal Balance { get; set; }
+        public int AccountNumber { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ BankingProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingProject
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
